24ff7c40351e496064e010b4edb4ffc1ee911b6e
RadekVlcek <radoslav.vlcek@gmail.com>
2019-04-18 22:03:22 +0200

    just saving

--- output.js
+++ output.js
@@ -1,20 +1,29 @@
-function fetchData(fd){
-    fetch(fd)
+renderData = [];
+
+function fetchData(fD){
+    fetch(fD)
     .then(function(res){
         return res.json();
     })
-    .then(function(ad){
-        getData(ad);
+    .then(function(aD){
+        getData(aD);
     });
 }
 
+// Obtain data in JSON format
 function getData(d){
     const mainData = d.data;
     const metaData = d.metadata;
     
     mainData.forEach((o) => {
-        console.log(o);
+        saveRow(o, mainData.length);
     });
+}
+
+function saveRow(rowData, length){
+    for(let i=0 ; i<length ; i++){
+        
+    }
 }
 
 window.onload = fetchData('./data.json');
